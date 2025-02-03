c18609676e16e2ab835a7aebab56ac54d0212386
Ciprian Bica <c.bica@student.utwente.nl>
2025-02-03 17:12:58 +0100

    day18 part 2 complete

--- day18/day18_part2.py
+++ day18/day18_part2.py
@@ -10,7 +10,6 @@ for lines in file:
         i = int(i)
         if i > max:
             max = i
-print("max is: ", max)
 
 max += 1
 mat = np.zeros((max, max, max))
@@ -23,8 +22,6 @@ with open("day18\\day18_input.txt") as file:
         n2 = int(l[1])
         n3 = int(l[2])
         mat[n1, n2, n3] = 1
-
-print(mat)
 
 
 def process_row(layer_matrix, layer_no):
@@ -74,14 +71,55 @@ def process_row(layer_matrix, layer_no):
                     faces += 1
                 if y == 0 or layer_matrix[x, y-1] == 0:
                     faces += 1
-            # SEE IF WE HAVE TRAPPED AIR
-            if layer_matrix[x, y] == 0 and layer_matrix[x+1, y] and layer_matrix[x-1, y]:
-                if layer_matrix[x, y+1] and layer_matrix[x, y-1]:
-                    if mat[layer_no-1, x, y] and mat[layer_no+1, x, y]:
-
     return faces
 
-
+# we flood the grid with 2s from the outside, this leaves the air pockets intact
+mat[0, 0, 0] = 2
+while True:
+    flag = 0
+    for x in range(mat.shape[0]):
+        for y in range(mat.shape[1]):
+            for z in range(mat.shape[2]):
+                # if any neighbor is 2 and block is 0
+                if x-1 >= 0 and mat[x, y, z] == 0 and mat[x-1, y, z] == 2:
+                    mat[x, y, z] = 2
+                    flag = 1
+                    continue
+                if y-1 >= 0 and mat[x, y, z] == 0 and mat[x, y-1, z] == 2:
+                    mat[x, y, z] = 2
+                    flag = 1
+                    continue
+                if z-1 >= 0 and mat[x, y, z] == 0 and mat[x, y, z-1] == 2:
+                    mat[x, y, z] = 2
+                    flag = 1
+                    continue
+                if x+1 < mat.shape[0] and mat[x, y, z] == 0 and mat[x+1, y, z] == 2:
+                    mat[x, y, z] = 2
+                    flag = 1
+                    continue
+                if y+1 < mat.shape[1] and mat[x, y, z] == 0 and mat[x, y+1, z] == 2:
+                    mat[x, y, z] = 2
+                    flag = 1
+                    continue
+                if z+1 < mat.shape[2] and mat[x, y, z] == 0 and mat[x, y, z+1] == 2:
+                    mat[x, y, z] = 2
+                    flag = 1
+                    continue
+    if flag == 0:
+        break
+# we fill in the air pockets with 1s
+x_c = 0
+for x in range(mat.shape[0]):
+    for y in range(mat.shape[1]):
+        for z in range(mat.shape[2]):
+            if mat[x, y, z] == 0:
+                mat[x, y, z] = 1
+# and we change all the 2s to 0 so the function from part 1 works
+for x in range(mat.shape[0]):
+    for y in range(mat.shape[1]):
+        for z in range(mat.shape[2]):
+            if mat[x, y, z] == 2:
+                mat[x, y, z] = 0
 result = 0
 for i in range(max):
     result += process_row(mat[i], i)
